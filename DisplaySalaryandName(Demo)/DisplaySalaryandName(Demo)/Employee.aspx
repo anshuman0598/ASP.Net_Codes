ee8464cc886ebfd2529b757728387fd11ce41408
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-14 09:41:06 +0530

    14-03-2022

---
+++ DisplaySalaryandName(Demo)/DisplaySalaryandName(Demo)/Employee.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="DisplaySalaryandName_Demo_.Employee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Emp" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DisplaySalaryandName(Demo)/DisplaySalaryandName(Demo)/Employee.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace DisplaySalaryandName_Demo_
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string source = "data source = DESKTOP-NARUTO\\SQL2019; initial catalog = Training2; integrated security = true;";
+            SqlConnection connect = new SqlConnection(source);
+            string query = "Select Name,Salary from Employee";
+            SqlCommand cmd = new SqlCommand(query, connect);
+            SqlDataReader dr;
+            connect.Open();
+            Emp.Text =" Database Connected ";
+            dr=cmd.ExecuteReader();
+            while(dr.Read()==true)
+            {
+                Emp.Text += " "+dr.GetValue(0)+" - "+dr.GetValue(1)+" || \n";
+            }
+            dr.Close();
+            connect.Close();
+            Emp.Text += " Database Disconnected ";
+
+        }
+    }
+}
