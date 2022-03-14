ea142d7888d2782b4c0b834efdad4b9de7155f83
anshuman0598 <dasanshuman5998@gmail.com>
2022-03-14 14:41:08 +0530

    14-03-2022

---
+++ DataReader(GridView)/DataReader(GridView)/GridViewDataReader.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GridViewDataReader.aspx.cs" Inherits="DataReader_GridView_.GridViewDataReader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" Height="457px" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="770px"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataSet(Demo)WebApplication/DataSet(Demo)WebApplication/DataSetDemo.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace DataSet_Demo_WebApplication
+{
+    public partial class DataSetDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetEmployeeData();
+        }
+
+        public void GetEmployeeData()
+        {
+            string source = "data source=DESKTOP-NARUTO\\SQL2019;initial catalog=Training2;integrated security=true;";
+            SqlConnection conn = new SqlConnection(source);
+            conn.Open();
+            string query = "Select * from Employee";
+            SqlCommand cmd = new SqlCommand(query,conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(query,conn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            EmployeeGrid.DataSource = ds;
+            EmployeeGrid.DataBind();
+            conn.Close();
+
+        }
+    }
+}
